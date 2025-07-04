8353539e5d703f5e72d2af5d1659fe353efb0c71
jeongu-97 <jung1w97@gmail.com>
2025-07-04 09:52:51 +0900

    Update next.config.mjs

--- CG_company/yoongdo/next.config.mjs
+++ CG_company/yoongdo/next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://192.168.1.138:8000/api/:path*'
+        destination: 'http://3.148.243.246:8000/api/:path*'
       }
     ];
   }
